0c51415fbf013c60c7544b4504fd7ad8d68e2b64
kirsten d <kirdanic@gmail.com>
2023-10-04 23:56:17 -0500

    Add User object with methods for interacting with API endpoints such as login, fetching users and new user registration to script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+// Define URL for API
+const BASE_URL = "http://localhost:8080/api";
+
+// Define an object for user with methods for API endpoint interaction
+const User = {
+  // Login
+  login: function (username, password) {
+    return fetch(`${BASE_URL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => response.json())
+      .catch((error) => console.error("Error:", error));
+  },
+
+  // Get all users
+  getAllUsers: function () {
+    return fetch(`${BASE_URL}/users`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .catch((error) => console.error("Error:", error));
+  },
+
+  // Register new user account
+  registerNewUserAccount: function (username, password) {
+    return fetch(`${BASE_URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => response.json())
+      .catch((error) => console.error("Error:", error));
+  },
+};
+
+// SAMPLE OF METHODS IN USE
+User.login("your_username", "your_password").then((data) => console.log(data));
+User.getAllUsers().then((users) => console.log(users));
+User.registerNewUserAccount("new_username", "new_password").then((data) =>
+  console.log(data)
+);
